f8d79b017318df60a3c4950d93a688aea461ce68
gizmore <gizmore@wechall.net>
2021-11-30 01:50:35 +0100

    6.11.0-r6218

---
+++ Module_Alcoholicers.php
@@ -0,0 +1,13 @@
+<?php
+namespace GDO\Alcoholicers;
+
+use GDO\Core\GDO_Module;
+
+/**
+ * @author gizmore
+ * @version 6.11.0
+ * @since 6.11.0
+ */
+class Module_Alcoholicers extends GDO_Module
+{
+}
